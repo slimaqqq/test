0071109a273afc635ac874a579cbd103a141ef47
phil.hardwick <philipchardwick@gmail.com>
2020-09-14 10:54:48 +0100

    car api outline

---
+++ src/main/java/com/eeveebank/simplespring/client/CarApiClient.java
@@ -0,0 +1,11 @@
+package com.eeveebank.simplespring.client;
+
+import com.eeveebank.simplespring.model.Car;
+
+import java.util.List;
+
+public interface CarApiClient {
+
+    List<Car> getAllCars();
+
+}
